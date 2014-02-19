5480f46c6157e3a53a7b374917d93ed6a4f88a5e
Stephen Richter <stephenrichter15@gmail.com>
2014-02-19 14:45:07 -0800

    Initial Commit.

---
+++ barfinder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  barfinder
+//
+//  Created by Stephen Richter on 2/19/2014.
+//  Copyright (c) 2014 Millennial Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ barfinder/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  barfinder
+//
+//  Created by Stephen Richter on 2/19/2014.
+//  Copyright (c) 2014 Millennial Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
